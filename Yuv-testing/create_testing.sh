f30500a0d11093995d35f560d8f22c78b8a2deef
yuval.dar <yuvaldar@gmail.com>
2025-03-02 17:43:20 +0200

    try

--- Yuv-testing/create_testing.sh
+++ Yuv-testing/create_testing.sh
@@ -3,6 +3,26 @@
 # will create 10 files with timestamps decreasing by 10 hours each.
 # so need to keep the first 4 and erase the follow 6.
 
+# Function to update the timestamp
+# if i < 3, then change time stamp to 1 day ago
+# if i <= 5, then change time stamp to 2 days ago
+# if i <= 7, then change time stamp to 3 days ago
+# if i <= 10, then change time stamp to 4 days ago
+update_time_stamp() {
+    i=$1
+    if [ $i -lt 3 ]; then
+        timestamp=$(date -d "1 days ago")
+    # elif [ $i -le 5 ]; then
+    #     timestamp=$(date -d "$current_time -$(( (i-1) * 10 + 1 * 24 )) hours" +"%Y%m%d%H%M")
+    # elif [ $i -le 7 ]; then
+    #     timestamp=$(date -d "$current_time -$(( (i-1) * 10 + 2 * 24 )) hours" +"%Y%m%d%H%M")
+    # elif [ $i -le 10 ]; then
+    #     timestamp=$(date -d "$current_time -$(( (i-1) * 10 + 3 * 24 )) hours" +"%Y%m%d%H%M")
+    fi
+    echo $timestamp
+}
+
+
 # Get the current date and time
 current_time=$(date +"%Y-%m-%d %H:%M:%S")
 
@@ -13,21 +33,21 @@ do
     touch "$filename"
 
     # calculate timestamp: decrease by (i-1) * 10 hours
-    # timestamp=$(date -d "$((-(i-1)*10)) hours ago" +"%Y%m%d%H%M.%S")
-    timestamp=$(date -d "1 day ago" +"%Y%m%d%H%M.%S")
 
     echo "yuval testing file number $i" > "$filename"
-    echo "fake time stamp is: ${timestamp}" >> "$filename"
 
     zip_name="fc-${filename}.zip"
     zip "$zip_name" "$filename"
     rm "$filename"
 
+    timestamp=update_time_stamp $i
     # set file timestamp to the updated one
     touch -t "$timestamp" "$zip_name"
 
     # echo "Created $filename with timestamp: $(date -d "$current_time -$(( (i-1) * 10 )) hours" +"%Y-%m-%d %H:%M:%S")"
 done
+
+
 
 # echo "10 files created with timestamps decreasing by 10 hours each."
 
